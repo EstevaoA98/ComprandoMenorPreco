22e2b54692059c74efbd6c9cb45cd34adacb72a5
Estevao Miranda <estevao.a98@gmail.com>
2025-07-25 18:10:19 -0300

    Injecao de independcia / conf Service

--- src/main/java/com/ListaPrecoBaixo/ComprandoMenorPreco/controller/ProdutoController.java
+++ src/main/java/com/ListaPrecoBaixo/ComprandoMenorPreco/controller/ProdutoController.java
@@ -1,5 +1,7 @@
 package com.ListaPrecoBaixo.ComprandoMenorPreco.controller;
 
+import com.ListaPrecoBaixo.ComprandoMenorPreco.service.PesquisaPrecoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/Lista-Compra")
 public class ProdutoController {
+
+    @Autowired
+    private PesquisaPrecoService pesquisaPrecoService;
+
+    //http://localhost:8080/Lista-Compra
     //GET
     @GetMapping
     public String RestTemplate(){
-        return "Lista de Compras!";
+        return pesquisaPrecoService.pesquisarPreco("Produto");
     }
 
 }
--- src/main/java/com/ListaPrecoBaixo/ComprandoMenorPreco/service/PesquisaPrecoService.java
+++ src/main/java/com/ListaPrecoBaixo/ComprandoMenorPreco/service/PesquisaPrecoService.java
@@ -1,4 +1,16 @@
 package com.ListaPrecoBaixo.ComprandoMenorPreco.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PesquisaPrecoService {
+
+    // implementar a lógica de pesquisa de preços
+    //RestTemplate para fazer chamadas a APIs externas
+
+    public String pesquisarPreco(String produto) {
+
+        //pesquisar o preço do produto
+        return "Preço do produto " + produto + " pesquisado com sucesso!";
+    }
 }
