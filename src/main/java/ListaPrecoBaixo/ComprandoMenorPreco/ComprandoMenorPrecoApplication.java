c5e74c0f3a3bb8ccfc1f540f08150a3928b3f6f1
EstevaoA98 <estevao.a98@gmail.com>
2025-07-25 09:09:31 -0300

    First commit

---
+++ src/main/java/ListaPrecoBaixo/ComprandoMenorPreco/ComprandoMenorPrecoApplication.java
@@ -0,0 +1,13 @@
+package ListaPrecoBaixo.ComprandoMenorPreco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprandoMenorPrecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprandoMenorPrecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ListaPrecoBaixo/ComprandoMenorPreco/ComprandoMenorPrecoApplicationTests.java
@@ -0,0 +1,13 @@
+package ListaPrecoBaixo.ComprandoMenorPreco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprandoMenorPrecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
